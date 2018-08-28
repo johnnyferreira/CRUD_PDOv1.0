d6b9d18c5e6c5ed2814247f09b5b166e8472c1b7
johnnyferreira <johnnyferreirabass@gmail.com>
2018-08-28 12:08:04 -0300

    crud_pdo_v1.0

---
+++ CRUD_PDOv1.0/GuiaUtilizacaoCRUD.php
@@ -0,0 +1,77 @@
+<?php
+	
+	include "Sql.php";
+
+	//OBS: para que o CRUD funcione corretamente é necessario configurar uma conexao na classe Config (Config.php).
+
+	$teste = new Sql(); //Instanciando o objeto Sql (CRUD)
+
+	//Como usar os metodos do CRUD_PDO
+
+	//	1 - Selecionar todos os campos e valores da tabela
+
+	$result = $teste->select("adm"); //Deve ser especificado o nome da tabela que se deseja obter os dados
+
+	foreach ($result as $value) {
+		echo "id adm = ".$value["id_adm"]."<br>";
+		echo "login = ".$value["login"]."<br>";
+		echo "senha = ".$value["senha"]."<br>";	
+	}
+
+	echo "<hr>";
+
+	// 2 - Selecionar somente um valor especifico
+	$condicao = "id_adm = 3233454644";
+	$result2 = $teste->selectWhere("adm",$condicao); //Nome da tabela + condicao
+
+	foreach ($result2 as $valor);
+
+	if(@$valor){
+		echo "Encontrado<br>";
+		echo "Login: ".$valor["login"]." Senha: ".$valor["senha"];
+	}else{
+		echo "Não existe";
+	}
+
+	echo "<hr>";
+
+	// 3 - Inserir dados em uma tabela
+
+	//Dados a serem inseridos
+	$dados = ["login"=>"johnnyferreira","senha"=>"123"];
+
+	$retorno = $teste->insert("adm", $dados); //Nome da tabela e array de dados.
+
+	if($retorno){
+		echo "inserido com sucesso";
+	}else{
+		echo "falha ao inserir"; //Essa falha pode ocorrer caso os nomes dos campos não correspondam com os do banco de dados
+	}
+
+	echo "<hr>";
+
+	// 4 - Atualizar dados de um campo - OBS: A funcao update não permite atualizar um campo sem uma condicao previamente especificada
+	$dados = ["senha"=>"567"];
+	$condicao = "id_adm = 234232342";
+
+	$retorno = $teste->update("adm", $dados, $condicao);
+
+	if($retorno){
+		echo "Atualizado com sucesso";
+	}else{
+		echo "falha ao atualizar"; //Essa falha pode ocorrer caso os nomes dos campos não correspondam com os do banco de dados ou caso não exista a condicao especificada
+	}
+
+	echo "<hr>";
+
+	// 5 - Deletar dados da tabela - OBS: A funcao delete não permite deletar um campo sem uma condicao previamente especificada
+	$condicao = "senha = 567";
+	$retorno = $teste->delete("adm", $condicao);
+
+	if($retorno){
+		echo "Deletado com sucesso";
+	}else{
+		echo "falha ao deletar"; //Essa falha pode ocorrer caso os nomes dos campos não correspondam com os do banco de dados ou caso não exista a condicao especificada
+	}
+
+
